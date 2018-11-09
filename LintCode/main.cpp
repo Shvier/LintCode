1204035629e6d8be79c79a5b2ba8ddf4a728cfe0
Shvier <daulvi@hotmail.com>
2018-11-09 21:00:57 +0800

    add a solution for a easy puzzle

--- LintCode/main.cpp
+++ LintCode/main.cpp
@@ -7,9 +7,36 @@
 //
 
 #include <iostream>
+#include <map>
+
+using namespace std;
+
+int longestPalindrome(string &s) {
+    // write your code here
+    map<char, int> hashTable;
+    for (char c : s) {
+        ++ hashTable[c];
+    }
+    
+    int result = 0;
+    bool hasSingle = false;
+    map<char, int>::iterator iter;
+    iter = hashTable.begin();
+    while (iter != hashTable.end()) {
+        if (iter->second % 2 == 0) {
+            result += iter->second;
+        } else {
+            result += iter->second - 1;
+            hasSingle = true;
+        }
+        ++ iter;
+    }
+    return hasSingle ? result + 1 : result;
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    string str = "abcbe";
+    std::cout << longestPalindrome(str);
     return 0;
 }
