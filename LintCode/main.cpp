31c944639e29158173d1faf57abbcb44d06694e7
Shvier <daulvi@hotmail.com>
2018-11-09 20:47:46 +0800

    init

---
+++ LintCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LintCode
+//
+//  Created by Shvier on 2018/11/9.
+//  Copyright © 2018 Shvier. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
